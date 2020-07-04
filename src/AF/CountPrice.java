88b31940cf1a01e79912a14646c98f2c112af888
af2yl <1820023574@qq.com>
2020-07-04 15:16:32 +0800

    修改之前的产品名称等参数

--- src/AF/CountPrice.java
+++ src/AF/CountPrice.java
@@ -1,108 +1,104 @@
 package AF;
+
 public class CountPrice {
-    private double num_apple;
-    private double num_orange;
-    private double num_banana;
-    private double num_grapefruit;
-    private double num_peach;
-    private double pri_apple;
-    private double pri_orange;
-    private double pri_banana;
-    private double pri_grapefruit;
-    private double pri_peach;
+    private double num_notebook;
+    private double num_case;
+    private double num_paper;
+    private double num_tape;
+    private double num_eraser;
+    private double pri_notebook;
+    private double pri_case;
+    private double pri_paper;
+    private double pri_tape;
+    private double pri_eraser;
     private double total_price;
 
-    public double getNum_apple() {
-        return num_apple;
+    public double getNum_notebook() {
+        return num_notebook;
     }
 
-    public void setNum_apple(double num_apple) {
-        this.num_apple = num_apple;
-        this.setPri_apple(num_apple * 5.0);
+    public void setNum_notebook(double num_notebook) {
+        this.num_notebook = num_notebook;
+        this.setPri_notebook(num_notebook * 5.0);
     }
 
-    public double getNum_orange() {
-        return num_orange;
+    public double getNum_case() {
+        return num_case;
     }
 
-    public void setNum_orange(double num_orange) {
-        this.num_orange = num_orange;
-        this.setPri_orange(num_orange * 3.0);
+    public void setNum_case(double num_case) {
+        this.num_case = num_case;
+        this.setPri_case(num_case * 3.0);
     }
 
-    public double getNum_banana() {
-        return num_banana;
+    public double getNum_paper() { return num_paper; }
 
+    public void setNum_paper(double num_paper) {
+        this.num_paper = num_paper;
+        this.setPri_paper(num_paper * 2.0);
     }
 
-    public void setNum_banana(double num_banana) {
-        this.num_banana = num_banana;
-        this.setPri_banana(num_banana * 2.0);
+    public double getNum_tape() {
+        return num_tape;
     }
 
-    public double getNum_grapefruit() {
-        return num_grapefruit;
+    public void setNum_tape(double num_tape) {
+        this.num_tape = num_tape;
+        this.setPri_tape(num_tape * 4.5);
     }
 
-    public void setNum_grapefruit(double num_grapefruit) {
-        this.num_grapefruit = num_grapefruit;
-        this.setPri_grapefruit(num_grapefruit * 4.5);
+    public double getNum_eraser() {
+        return num_eraser;
     }
 
-    public double getNum_peach() {
-        return num_peach;
+    public void setNum_eraser(double num_eraser) {
+        this.num_eraser = num_eraser;
+        this.setPri_eraser(num_eraser * 5.5);
     }
 
-    public void setNum_peach(double num_peach) {
-        this.num_peach = num_peach;
-        this.setPri_peach(num_peach * 5.5);
+    public double getPri_notebook() {
+        return pri_notebook;
     }
 
-    public double getPri_apple() {
-        return pri_apple;
+    public void setPri_notebook(double pri_notebook) {
+        this.pri_notebook = pri_notebook;
     }
 
-    public void setPri_apple(double pri_apple) {
-        this.pri_apple = pri_apple;
+    public double getPri_case() {
+        return pri_case;
     }
 
-    public double getPri_orange() {
-        return pri_orange;
+    public void setPri_case(double pri_case) {
+        this.pri_case = pri_case;
     }
 
-    public void setPri_orange(double pri_orange) {
-        this.pri_orange = pri_orange;
+    public double getPri_paper() {
+        return pri_paper;
     }
 
-    public double getPri_banana() {
-        return pri_banana;
+    public void setPri_paper(double pri_paper) {
+        this.pri_paper = pri_paper;
     }
 
-    public void setPri_banana(double pri_banana) {
-        this.pri_banana = pri_banana;
+    public double getPri_tape() {
+        return pri_tape;
     }
 
-    public double getPri_grapefruit() {
-        return pri_grapefruit;
+    public void setPri_tape(double pri_tape) {
+        this.pri_tape = pri_tape;
     }
 
-    public void setPri_grapefruit(double pri_grapefruit) {
-        this.pri_grapefruit = pri_grapefruit;
+    public double getPri_eraser() {
+        return pri_eraser;
     }
 
-    public double getPri_peach() {
-        return pri_peach;
-    }
-
-    public void setPri_peach(double pri_peach) {
-        this.pri_peach = pri_peach;
+    public void setPri_eraser(double pri_eraser) {
+        this.pri_eraser = pri_eraser;
     }
 
     public double getTotal_price() {
-        this.total_price = this.getPri_apple() + this.getPri_banana()
-                + this.getPri_grapefruit() + this.getPri_grapefruit()
-                + this.getPri_orange() + this.getPri_peach();
+        this.total_price = this.getPri_notebook() + this.getPri_paper()
+                + this.getPri_tape() + this.getPri_case() + this.getPri_eraser();
         return total_price;
     }
 }
-
